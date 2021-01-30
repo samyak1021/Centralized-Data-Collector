45b1f79f821aca0563b51e1f03d2d9acceb36de3
Samyak <samdalal1021@gmail.com>
2021-01-30 15:30:46 +0530

    Fixed errors

--- code.py
+++ code.py
@@ -3,12 +3,14 @@ import json
 import os
 from datetime import datetime
 response = requests.get("https://api.data.gov.in/resource/28584657-445c-4ca0-857c-de1ae6a4888c?api-key=579b464db66ec23bdd0000019b990dd666564ef24abe53890cb520c3&format=json&offset=0&limit=500")
-last_updated = []
+last_updated = open("updated_date.txt","r+")
 json_data = response.json()
-last_updated_date = json_data["updated_date"]
-last_updated.append(last_updated_date)
-length = len(last_updated)
-if length != 1 and last_updated[length-1] != last_updated[length-2]:
+last_updated_date = last_updated.read()
+current_date = json_data["updated_date"]
+if current_date != last_updated_date:
+    last_updated.seek(0)
+    last_updated.truncate(0)
+    last_updated.write(current_date)
     records = json_data['records']
     timestr = datetime.now().strftime("%Y%m%d-%H%M%S-%f")
     dir_path = os.path.join('F:\Data') #Folder Where you want to save data
